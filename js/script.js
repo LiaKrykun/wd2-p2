35c1f12927957d490d142dd1405fd5a250756032
LiaKrykun <96320402+LiaKrykun@users.noreply.github.com>
2025-03-23 22:31:28 -0700

    Taylor (#3)
    * created styles for New Products, By Benefit, and Best Sellers Sections. Added Html to sections to as closely as possible resemble delegated sections from Two Islands website
    
    * saved media query changes to best sellers sections
    
    * Remove old js
    
    * remove unused css folder
    
    * fix css conficts and dublicates
    
    * small tab missing
    
    ---------
    
    Co-authored-by: Taylor Doyle <genericaddress@email.ca>

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const categoryItems = document.querySelectorAll(".category-item");
+    const categoryImage = document.querySelector(".category-image");
+    const defaultImage = categoryImage.src;
 
-// Running images in section FOLLOW US
-document.getElementById("scrollLeft").addEventListener("click", function() {
-    document.querySelector(".journal-cards-container").scrollBy({ left: -300, behavior: "smooth" });
-});
+    categoryItems.forEach((item) => {
+      item.addEventListener("mouseenter", function () {
+        const imageUrl = this.dataset.image;
+        categoryImage.style.opacity = "0";
+        setTimeout(() => {
+          categoryImage.src = imageUrl;
+          categoryImage.style.opacity = "1";
+        }, 300);
+        this.querySelector(".category-card").classList.add("active");
+      });
 
-document.getElementById("scrollRight").addEventListener("click", function() {
-    document.querySelector(".journal-cards-container").scrollBy({ left: 300, behavior: "smooth" });
-});
+      item.addEventListener("mouseleave", function () {
+        categoryImage.style.opacity = "0";
+        setTimeout(() => {
+          categoryImage.src = defaultImage;
+          categoryImage.style.opacity = "0.95";
+        }, 300);
+        this.querySelector(".category-card").classList.remove("active");
+      });
+    });
+  });
